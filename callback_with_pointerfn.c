8e28fde0dc960eca980e6801be2e2ff9dcbc95a2
Chethan <chethan7707@gmail.com>
2026-03-03 14:23:47 +0530

    Added a callback function program with pointer functions

---
+++ callback_with_pointerfn.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+
+void hello()
+{
+    printf("Hello World\n");
+}
+
+void callback(void (*ptr1)( ),int (*ptr2)(int,int)){
+    printf("Calling a function with its pointer\n");
+
+    (*ptr1)();  // calling the callback func.
+}
+
+
+
+int sum(int a, int b)
+{
+    return a+b;
+}
+int main(){
+    void (*ptr)()=hello;
+    int (*ptr2)(int,int)=sum;
+    printf("%d\n",(*ptr2)(67,69));
+    (*ptr)();
+    callback(ptr,ptr2);
+
+    return 0;
+
+}
