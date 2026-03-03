94cc29f60a1a86a618e1ac812f78d08fe9d6e7c0
Chethan <chethan7707@gmail.com>
2026-03-03 14:48:25 +0530

    callback with pointer function with arguments

---
+++ callback_with_pointerfn_arguments.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int add(int a,int b)
+{
+    return a+b;
+}
+
+int subtract(int a,int b)
+{
+    return a-b;
+}
+
+int (*getoperation(char op))(int,int){
+    if(op=='+') {return &add;}
+    else if(op=='-') {return &subtract;}
+    else {return NULL;}
+}
+
+int main()
+{
+    //get the func pointer based on op
+
+    int (*operation)(int,int)=getoperation('+');
+    if (operation) 
+    printf("Result : %d\n",operation(7,9));
+    else 
+    printf("Invalid operation\n");
+
+}
+
